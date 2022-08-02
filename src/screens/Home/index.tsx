ecf2776c65c63b0244c66b56130b4b7b95433a38
Agacy junior <juniormartinssom@gmail.com>
2022-08-02 19:43:38 -0300

    Criando o form

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,19 +1,29 @@
-import { Text, View, TextInput } from "react-native";
+import { Text, View, TextInput, TouchableOpacity } from "react-native";
 import { styles } from "./styles";
 
 export default function Home() {
+  function handleParticipantAdd() {
+    console.log("Clicou no botão de Adicionar!");
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do evento</Text>
 
       <Text style={styles.eventDate}>Sexta, 4 de novembro de 2022.</Text>
-    
-      <TextInput 
-        style={styles.input} 
-        placeholder="Nome do participante"
-        placeholderTextColor="#6B6B6B"
-        keyboardType="numeric" 
-      />
+
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+          keyboardType="numeric"
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -17,11 +17,31 @@ export const styles = StyleSheet.create({
     fontSize: 16,
   },
   input: {
+    flex: 1,
     height: 56,
     backgroundColor: "#1F1E25",
     borderRadius: 5,
     color: "#FDFCFE",
     padding: 16,
     fontSize: 16,
+    marginRight: 12,
+  },
+  button: {
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: "#31CF67",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    color: "#FFF",
+    fontSize: 24,
+  },
+  form: {
+    width: '100%',
+    flexDirection: 'row',
+    marginTop: 36,
+    margintBottom: 42,
   },
 });
